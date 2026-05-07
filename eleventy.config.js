b5ed8c97411d9fd34c49087785a6cfe509ef367b
Bruno Sastre <brfignoni@gmail.com>
2026-05-07 00:22:46 -0300

    Update eleventy.config.js

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,9 +1,22 @@
 import syntaxHighlight from "@11ty/eleventy-plugin-syntaxhighlight";
 import rssPlugin from "@11ty/eleventy-plugin-rss";
+import { EleventyI18nPlugin } from "@11ty/eleventy";
 
 export default function (eleventyConfig) {
   eleventyConfig.addPlugin(syntaxHighlight);
   eleventyConfig.addPlugin(rssPlugin);
+  eleventyConfig.addPlugin(EleventyI18nPlugin, { defaultLanguage: "es" });
+
+  eleventyConfig.addFilter("getTranslations", function (allPages, translationKey, currentLang) {
+    if (!translationKey) return [];
+    return allPages.filter(
+      (page) => page.data.translationKey === translationKey && page.data.lang !== currentLang
+    );
+  });
+
+  eleventyConfig.addFilter("postsByLang", function (collection, lang) {
+    return collection.filter((p) => p.data.lang === lang);
+  });
 
   eleventyConfig.addPassthroughCopy("src/assets");
 
